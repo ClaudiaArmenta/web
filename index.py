59e9b77a6c949bd3b66ecab9b870a9287bb52cae
SarahiArmenta <54731228+SarahiArmenta@users.noreply.github.com>
2022-11-18 14:33:57 -0600

    versionRegresion

--- index.py
+++ index.py
@@ -9,6 +9,8 @@ from sklearn.tree import DecisionTreeClassifier
 from sklearn import tree
 from sklearn.ensemble import RandomForestClassifier
 from sklearn import metrics
+from sklearn.linear_model import LinearRegression 
+from sklearn import linear_model
 
 from flask import Flask, render_template, request
 from flask_bootstrap import Bootstrap
@@ -19,11 +21,30 @@ X= pd.DataFrame(df,
                columns=["Nivel Designado","Mes R","Año R","Empresa"])
 y=df['diferencia'] #Target
 X_train, X_test, y_train, y_test = train_test_split(X, y,random_state = 5)
-model = RandomForestClassifier(n_estimators=1000)
+#Forest
+model = RandomForestClassifier(n_estimators=1000)#Forest
 model.fit(X_train, y_train)
 ypred = model.predict(X_test)
+
+#predecir para forest
 def predecir(n1,n2,n3,n4):
   return(model.predict(X=[[n1,n2,n3,n4]]))
+
+#para regresion
+regr = linear_model.LinearRegression()
+X1= pd.DataFrame(df,
+               columns=["Nivel Designado"])
+
+X1_train, X1_test, y1_train, y1_test = train_test_split(X1, y,random_state = 5)
+
+# Entrenamos nuestro modelo
+modelo= regr.fit(X1_train, y1_train)
+ 
+# Hacemos las predicciones que en definitiva una línea (en este caso, al ser 2D)
+y1_pred = regr.predict(X1_train)
+#predecirR
+def predecirR(n1):
+  return( regr.predict(X=[[n1]]))
 
 app= Flask(__name__)
 
@@ -31,7 +52,7 @@ app= Flask(__name__)
 def home():
     return render_template('index.html')
 
-@app.route('/predictivo', methods=["GET","POST"])
+@app.route('/predictivoForest', methods=["GET","POST"])
 def about():
     if request.method == 'POST':
         nivel= request.form['nivel'] 
@@ -41,9 +62,18 @@ def about():
         result=predecir(nivel,mes, year, empresa)
         
         return render_template('resul.html', result=result)
-        #return f'<div class="alert alert-primary" role="alert">Los días que se va tardar son:'+ str(result) +'</div>'
     else :
-        return render_template('predictivo.html')
+        return render_template('predictivoForest.html')
+
+@app.route('/predictivoRegresion', methods=["GET","POST"])
+def aboutR():
+    if request.method == 'POST':
+        nivelr= request.form['nivelr'] 
+        resultR=predecirR(nivelr)
+        
+        return f'<div class="alert alert-primary" role="alert">'+ str(resultR) +'</div>'
+    else :
+        return render_template('predicitvoRegresion.html')
 
 if __name__=='__main__':
     app.run(debug=False,host='0.0.0.0')
