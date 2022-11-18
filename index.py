675b89332f7e82ac291202611fc731fa16f0856c
SarahiArmenta <54731228+SarahiArmenta@users.noreply.github.com>
2022-11-18 14:42:25 -0600

    final

--- index.py
+++ index.py
@@ -2,15 +2,12 @@ from sklearn import datasets
 import pandas as pd
 import numpy as np 
 from sklearn.model_selection import train_test_split
-import matplotlib.pyplot as plt
-import numpy
 import seaborn as sns
 from sklearn.tree import DecisionTreeClassifier
 from sklearn import tree
 from sklearn.ensemble import RandomForestClassifier
 from sklearn import metrics
-from sklearn.linear_model import LinearRegression 
-from sklearn import linear_model
+
 
 from flask import Flask, render_template, request
 from flask_bootstrap import Bootstrap
@@ -30,21 +27,6 @@ ypred = model.predict(X_test)
 def predecir(n1,n2,n3,n4):
   return(model.predict(X=[[n1,n2,n3,n4]]))
 
-#para regresion
-regr = linear_model.LinearRegression()
-X1= pd.DataFrame(df,
-               columns=["Nivel Designado"])
-
-X1_train, X1_test, y1_train, y1_test = train_test_split(X1, y,random_state = 5)
-
-# Entrenamos nuestro modelo
-modelo= regr.fit(X1_train, y1_train)
- 
-# Hacemos las predicciones que en definitiva una línea (en este caso, al ser 2D)
-y1_pred = regr.predict(X1_train)
-#predecirR
-def predecirR(n1):
-  return( regr.predict(X=[[n1]]))
 
 app= Flask(__name__)
 
@@ -65,14 +47,8 @@ def about():
     else :
         return render_template('predictivoForest.html')
 
-@app.route('/predictivoRegresion', methods=["GET","POST"])
+@app.route('/predictivoRegresion')
 def aboutR():
-    if request.method == 'POST':
-        nivelr= request.form['nivelr'] 
-        resultR=predecirR(nivelr)
-        
-        return f'<div class="alert alert-primary" role="alert">'+ str(resultR) +'</div>'
-    else :
         return render_template('predicitvoRegresion.html')
 
 if __name__=='__main__':
